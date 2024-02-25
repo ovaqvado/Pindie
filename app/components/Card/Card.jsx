ff80647aa5f275f096ade7b0766431c5499b6cd1
ovaqvado <likeriklohhg@gmail.com>
2024-02-25 20:09:38 +0300

    useState hook

--- app/components/Card/Card.jsx
+++ app/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import "./Card.module.css";
+import Styles from "./Card.module.css";
 
 export const Card = (props) => {
   return (
--- app/page.js
+++ app/page.js
@@ -1,24 +1,16 @@
+import { getGamesByCategory } from "./Data/data-utils";
 import { Banner } from "./components/Banner/Banner";
 import { Promo } from "./components/Promo/Promo";
 import { CardList } from "./components/CardList/CardList";
-import { NewCardsFragment } from "./components/CardList/NewCardsFragment";
-import { PopularCardsFragment } from "./components/CardList/PopularCardsFragment";
 
 export default function Home() {
+  const popularGames = getGamesByCategory("popular");
+  const newGames = getGamesByCategory("new");
   return (
     <main className="main">
       <Banner />
-
-      <CardList id="popular" title="Популярные">
-        <PopularCardsFragment />
-      </CardList>
-
-      <CardList id="new" title="Новые">
-        <NewCardsFragment />
-      </CardList>
-
-
-
+      <CardList id="popular" title="Популярные" data={popularGames} />
+      <CardList id="new" title="Новые" data={newGames} />
       <Promo />
     </main>
   );
